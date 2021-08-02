8361e12aef70b0ae1aaa1d062ef4309b22be4edd
konovnina <ea.knvn@gmail.com>
2021-08-02 11:23:51 +0400

    Add lesson 3

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let money, income, addExpenses, deposit, mission, period;
 
-// alert('Alert message');
-// console.log('Console message');
-
-//1
-money = 50000;
+//Получение данных
+money = +prompt('Ваш месячный доход: ', 50000);
 income = "Фриланс";
-addExpenses = "Кредит, Коммуналка, Еда, Здоровье, Развлечения, Непредвиденные расходы";
-deposit = true;
+
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую:', 'Кредит, Коммуналка, Еда');
+
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+let expenses1 = prompt('Введите обязательную статью расходов:', 'Коммунальные платежи');
+let amount1 = +prompt('Во сколько это обойдется?', 10000);
+let expenses2 = prompt('Введите обязательную статью расходов:', 'Еда');
+let amount2 = +prompt('Во сколько это обойдется?', 10000);
+
 mission = 100000;
 period = 10;
 
-//2
+//Вывод в консоль из предыдущего задания
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit)
@@ -22,5 +27,26 @@ console.log(`Цель заработать ${mission} рублей`);
 addExpenses = addExpenses.toLowerCase().split(', ');
 console.log(addExpenses);
 
-let budgetDay = Math.round(money / 30);
+//Расчет бюджета на месяц
+let budgetMonth = money - (amount1 + amount2);
+console.log('Бюджет на месяц: ', budgetMonth);
+
+//За какой срок будет достигнута цель
+period = Math.round(mission / budgetMonth);
+console.log('Цель будет достигнута за ', period, ' месяца(-ев)');
+
+
+//Расчет бюджета на день
+let budgetDay = Math.floor(budgetMonth / 30);
 console.log(budgetDay);
+
+//Расчет уровня дохода
+if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if ((budgetDay >= 600) && (budgetDay < 1200)) {
+  console.log('У вас средний уровень дохода');
+} else if ((budgetDay >= 0) && (budgetDay < 600)) {
+  console.log('К сожалению, у вас уровень дохода ниже среднего');  
+} else {
+  console.log('Что-то полшло не так');
+}
